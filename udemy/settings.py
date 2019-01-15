f98decb014a388336e91382129e4b2c3703c573c
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2019-01-15 05:05:31 -0300

    Update docker-compose

--- udemy/settings.py
+++ udemy/settings.py
@@ -27,7 +27,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = []
 
